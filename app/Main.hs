23806d8d980872d57760b0bd0812c3c8f82a8959
Carlos <carlozalejandro98@gmail.com>
2022-02-04 16:57:18 -0300

    finishing

--- app/Main.hs
+++ app/Main.hs
@@ -10,61 +10,57 @@ import Objetivos
 import FuncionAgentes
 import MovimientoNinhos
 
+-- instalar este software para imprimir los emojis en la consola
 -- sudo apt install fonts-emojione
 
 
-sem = 109009211157976
-tableroSemilla = iniciarTablero 6 6 sem
-conNinhos = generarNinhos tableroSemilla 3
-conRobots = generarRobots conNinhos 2
-conCuna = generarCunas conRobots 3
-conObstaculos = generarObstaculos conCuna 3
-conSuciedad = generarSuciedades conObstaculos 0
+--CAmbiar estas variables para obtener una simulacion diferente
+cantNinhos = 3
+cantRobots = 8
+cantSuciedad = 3
+cantObstaculos = 1
+anchoTablero = 7
+largoTablero = 7
+sem = 1
+tAmbiente = 3
+cantIteraciones = 30
+---------------------------------------------------------------------------
 
 
-cant = 30
+tableroSemilla = iniciarTablero largoTablero anchoTablero sem
+conNinhos = generarNinhos tableroSemilla cantNinhos
+conRobots = generarRobots conNinhos cantRobots
+conCuna = generarCunas conRobots cantNinhos
+conObstaculos = generarObstaculos conCuna cantObstaculos
+conSuciedad = generarSuciedades conObstaculos cantSuciedad
 
-pintura = iteracionesPintar conSuciedad cant
-tablero = iteracionesTablero conSuciedad cant
---obs = escribir tablero (crearObstaculo 2 3)
-ma = robotPuedeMoverse tablero (crearRobot 4 0) 4 2
---md = mover2Derecha ma (crearRobot 1 2)
-t = getCuadricula tablero (crearNinho 0 0)
-nt = getNinhosCuadricula tablero t []
-qn = moverNinhosDeCuadricula tablero nt
+simulacion = iteracionesTablero conSuciedad 1 [conSuciedad]
+simulacionPintura = pintarArrayTablero simulacion 0
+
 main :: IO ()
 main =  do
-    putStrLn pintura
-    -- print t
-    -- print nt
-    -- putStrLn (pintarTablero qn)
-    --print "\n"
-    --  print (robotPuedeMoverse tablero (crearRobot 3 0) 0 (-2))
-    --putStrLn (pintarTablero tablero)
-    --  print "\n\n"
-    --print ma
-    --  print "\n\n"
-    --  putStrLn (pintarTablero md)
-    -- putStrLn (pintarTablero md)
-    -- putStrLn (pintarTablero md2)
-    
-    --print (objetivos tablero)
-    
-    
-    
-    --putStrLn (pintarTablero (iteracion conSuciedad))
-
-iteracionesPintar :: Tablero -> Int -> String
-iteracionesPintar t i 
-    | i == 0 = (show (objetivos t)) ++ "\n" ++ pintarTablero t
-    | otherwise = let nuevoTablero = iteracion t
-                      movNinhos = turnoAmbiente nuevoTablero
-                 in (show (objetivos t)) ++ "\n" ++ pintarTablero t ++ (iteracionesPintar movNinhos (i-1))
+    putStrLn simulacionPintura
 
 
-iteracionesTablero :: Tablero -> Int -> Tablero
-iteracionesTablero t i 
-    | i == 0 = t
+
+pintarArrayTablero :: [Tablero] -> Int -> String
+pintarArrayTablero tableros index
+    | index == length tableros = ""
+    | otherwise = (show (index+1)) ++ turnoAmb ++ (pintarTablero tableroIndex) ++ pintarArrayTablero tableros (index + 1)
+    where
+        turnoAmb = if mod index tAmbiente == 0 && index /= 0
+                   then " (Turno del Ambiente)\n"
+                   else "\n"
+        tableroIndex = indexar tableros index 
+
+iteracionesTablero :: Tablero -> Int -> [Tablero] -> [Tablero]
+iteracionesTablero t i acumulado
+    | i == (cantIteraciones) = acumulado
     | otherwise = let nt = iteracion t
-                   in iteracionesTablero nt (i-1)  
+                      
+                      turnoAmb = if (mod i tAmbiente) == 0
+                                 then turnoAmbiente nt
+                                 else nt
+                      nuevoAcumulado = agregar turnoAmb acumulado
+                   in iteracionesTablero turnoAmb (i+1) nuevoAcumulado 
                  
